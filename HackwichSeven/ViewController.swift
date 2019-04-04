3432f585786ec0b2e64459ffeb82ddcc5e429b62
Keanna <keannakr@hawaii.edu>
2019-04-04 10:25:35 -1000

    completed Hackwich Seven

--- HackwichSeven/ViewController.swift
+++ HackwichSeven/ViewController.swift
@@ -7,14 +7,32 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class ViewController: UIViewController {
-
+   
+    @IBOutlet var mapView: MKMapView!
+    
+    let initialLocation = CLLocation(latitude: 21.361888 , longitude: -158.055725)
+    let regionRadius: CLLocationDistance = 10000
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        centerMapLocation(location: initialLocation)
+      
+        let restaurantOne = Restaurant(title: "Kalapawai", type: "American", coordinate: CLLocationCoordinate2D(latitude: 21.346470, longitude: -158.080098))
+        mapView.addAnnotation(restaurantOne)
     }
 
+    func centerMapLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegion.init (center: location.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        
+        
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
 
 }
 
